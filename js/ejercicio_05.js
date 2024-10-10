e5c965313ffa548ac60535beafc669ebfa2d9f13
fariasdsm <230389@utxicotepec.edu.mx>
2024-10-10 15:27:50 -0600

    Practica 7
    Terminacion del ejercicio 5

--- js/ejercicio_05.js
+++ js/ejercicio_05.js
@@ -1,6 +1,6 @@
 const bg =
-  "linear-gradient(11deg, rgba(200,150,360,1) 0%, rgba(260,129,12,1) 33%, rgba(255,19,260,1) 86%)";
-const style_console = `background: ${bg}; color: white; border-radius: 6px; padding: 4px; font-size: 1.0rem; font-weight: bold`;
+  "linear-gradient(135deg, rgba(63,94,251,1) 0%, rgba(252,70,107,1) 50%, rgba(0,212,255,1) 100%)";
+const style_console = `background: ${bg}; color: white; border-radius: 8px; padding: 6px; font-size: 1.1rem; font-weight: bold; text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.3)`;
 
 //Personalización de las salidas a consola
 
@@ -127,6 +127,172 @@ let asignaGeneracion = (anioNacimiento) => {
   }
 };
 
-console.log();
+console.log(
+  `Dado que nací en el año 2001 soy de la generación ${asignaGeneracion(2001)}`
+);
 
-//
+console.log("%c4.- Manejo de Escepciones ( TRY / CATCH )", style_console);
+
+//En algunos ocasiones existiran errores que no son culpa del programa, si no del usuario, la red, el so o incluso de un middleware, pero que si duda debemos controlar para evitar las fallas de ejecución.
+
+console.log("Intentamos dividir : 0/10, el resultado es: ");
+try {
+  let result = 0 / 10;
+  console.log(result);
+} catch (error) {
+  console.log("Ocurrio un error: " + error.message);
+}
+
+console.log("Intentamos dividir : 10/0, el resultado es: ");
+try {
+  let result = 10 / 0;
+  console.log(result);
+} catch (error) {
+  console.log("Ocurrio un error: " + error.message);
+}
+
+console.log("Intentamos dividir : a/0, el resultado es: ");
+try {
+  //intentamos
+  let result = "a" / 10; //dividir una letra entre 0
+  console.log(result);
+} catch (error) {
+  console.log("Ocurrio un error: " + error.message);
+}
+
+console.log("Intentamos dividir : la variable a/0, el resultado es: ");
+try {
+  //intentamos
+  let result = a / 0; //dividir una variable no definida
+  console.log(result);
+} catch (error) {
+  console.log("Ocurrio un error: " + error.message);
+}
+
+console.log(
+  "Intentamos dividir : el valor de la variable x/ entre el valor de la variable y, el resultado es:"
+);
+try {
+  // Intentamos
+  let x = 8,
+    y = 2,
+    result = x / y; //dividir una variable no definida
+  console.log(result);
+} catch (error) {
+  console.log("Ocurrio un error: " + error.message);
+}
+
+console.log("%c5.- Control de Ciclos ( BREAK / CONTINUE ))", style_console);
+
+//En algunas ocasiones será importante detener un ciclo de manera abrupta para controlar casos especiales en un ciclo...
+
+console.log("Vamos a contar del 1 al 10...");
+for (let num = 1; num < 11; num++) {
+  console.log(num);
+}
+
+console.log(
+  "Ahora necesitamos que si llegas al 7 pares de contar... suponiendo que solo por hoy es de mala suerte..."
+);
+for (num = 1; num < 11; num++) {
+  if (num == 7) break;
+  else console.log(num);
+}
+
+console.log(
+  "Ahora necesitamos que si llega al 7 te saltes ese numero y continues..."
+);
+for (num = 1; num < 11; num++) {
+  if (num == 7) {
+    continue;
+  }
+  console.log(num);
+}
+
+console.log("%c6. Ciclo Iterativo - (FOR) ", style_console);
+
+//Recorre demanera iterativa (i), de incremental a decremental
+
+console.log("Los dias de la semana son en orden ascendente: ");
+
+console.log("Los días de la semana son en ordenes ascendentes son: ");
+let dias = [
+  "Domingo",
+  "Lunes",
+  "Martes",
+  "Miércoles",
+  "Jueves",
+  "viernes",
+  "Sábado",
+];
+
+for (let i = 0; i < dias.length; i++) {
+  console.log(dias[i]);
+}
+console.log("Ahora vamos a imprimir los meses en orden descendente...");
+const meses = [
+  "Enero",
+  "Febrero",
+  "Marzo",
+  "Abril",
+  "Mayo",
+  "Junio",
+  "Julio",
+  "Agosto",
+  "Septiembre",
+  "Octubre",
+  "Noviembre",
+  "Diciembre",
+];
+
+for (let i = meses.length - 1; i >= 0; i--) {
+  console.log(meses[i]);
+}
+
+console.log("%c7.- Ciclo Condicionales - (WHILE))", style_console);
+//Estos ciclos (BUCLE) depende de una condición para continuar ejecutandose
+
+let finDeSemana = false;
+let mensaje = "";
+let j = 0;
+
+while (j < dias.length) {
+  switch (
+    j //Usamos "j" para que coincide con los casos
+  ) {
+    case 0:
+      finDeSemana = true;
+      mensaje = "Domingo de procastinar";
+      break;
+    case 1:
+      finDeSemana = false;
+      mensaje = "San Lunes a chambiar...";
+      break;
+    case 2:
+      finDeSemana = false;
+      mensaje = "Martes ni te cases, ni te embarques";
+      break;
+    case 3:
+      finDeSemana = false;
+      mensaje = "Ombligo de la semana, sigues chambeando...";
+      break;
+    case 4:
+      finDeSemana = false;
+      mensaje = "JUEVES de racismo...";
+      break;
+    case 5:
+      finDeSemana = false;
+      mensaje = "VIEeeEEEEEEeeeernes, disfruta el día";
+      break;
+    case 6:
+      finDeSemana = true;
+      mensaje = "Sábado, diviértete";
+      break;
+  }
+
+  if (!finDeSemana) {
+    console.log(`Día: ${j} ${dias[j]}`);
+    console.log(`Mensaje del día ${mensaje}`);
+  }
+  j++;
+}
